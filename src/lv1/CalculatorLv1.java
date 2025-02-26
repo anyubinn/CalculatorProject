b81cbdc553853cd6cb62cf1fdc7d0840d35faf4d
anyubinn <anyubin0724@naver.com>
2025-02-26 14:47:59 +0900

    feat: 에러 처리 기능 추가

--- src/lv1/CalculatorLv1.java
+++ src/lv1/CalculatorLv1.java
@@ -1,5 +1,6 @@
 package lv1;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class CalculatorLv1 {
@@ -8,6 +9,7 @@ public class CalculatorLv1 {
     private int num2;
     private char operator;
     private int result;
+    private List<Character> operatorArr = List.of('+', '-', '*', '/');
     private Scanner sc = new Scanner(System.in);
 
     public void startCalculate() {
@@ -22,32 +24,44 @@ public class CalculatorLv1 {
         System.out.print("양의 정수 두 개를 입력하세요 (0부터 입력 가능): ");
         num1 = sc.nextInt();
         num2 = sc.nextInt();
-        System.out.println("입력한 값: " + num1 + ", " + num2);
+        if (num1 < 0 || num2 < 0) {
+            System.out.println("양의 정수만 입력이 가능합니다. 다시 입력해주세요.");
+            inputNumber();
+        }
     }
 
     // 사칙연산 기호를 입력받는 메서드
     private void inputOperator() {
-        sc.nextLine(); // 버퍼 비우기
         System.out.print("사칙연산 기호를 입력하세요 (+, -, *, / 가능): ");
-        operator = sc.nextLine().charAt(0);
-        System.out.println("입력한 기호: " + operator);
+        operator = sc.next().charAt(0);
+        if (!operatorArr.contains(operator)) {
+            System.out.println("잘못된 사칙연산 기호입니다. 다시 입력해주세요.");
+            inputOperator();
+        }
     }
 
     // 입력된 정수와 연산 기호를 사용해 계산하는 메서드
     private int calculateResult() {
-        switch(operator) {
-            case '+':
-                result = num1 + num2;
-                break;
-            case '-':
-                result = num1 - num2;
-                break;
-            case '*':
-                result = num1 * num2;
-                break;
-            case '/':
-                result = num1 / num2;
-                break;
+        try {
+            switch (operator) {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    if (num2 == 0) {
+                        throw new ArithmeticException("0으로 나눌 수 없습니다.");
+                    }
+                    result = num1 / num2;
+                    break;
+            }
+        } catch (ArithmeticException e) {
+            System.out.println(e.getMessage());
         }
         return result;
     }

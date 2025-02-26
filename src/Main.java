4d73518429a769112bc7ecd139b4b7d58a785743
anyubinn <anyubin0724@naver.com>
2025-02-26 15:31:09 +0900

    refactor: lv1/Main.class 삭제

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import lv1.CalculatorLv1;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        CalculatorLv1 cal = new CalculatorLv1();
+        cal.startCalculate();
     }
 }
--- src/lv1/Main.java
+++
@@ -1,9 +0,0 @@
-package lv1;
-
-public class Main {
-
-    public static void main(String[] args) {
-        CalculatorLv1 cal = new CalculatorLv1();
-        cal.startCalculate();
-    }
-}

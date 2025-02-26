a5e00b12001aa89ae2be635c4f0ddcb38fee671d
anyubinn <anyubin0724@naver.com>
2025-02-26 14:02:46 +0900

    입력된 정수와 연산 기호를 사용해 계산하는 기능 추가

--- src/lv1/CaculatorLv1.java
+++ src/lv1/CaculatorLv1.java
@@ -7,11 +7,14 @@ public class CaculatorLv1 {
     private int num1;
     private int num2;
     private char operator;
+    private int result;
     private Scanner sc = new Scanner(System.in);
 
     public void startCalculate() {
         inputNumber();
         inputOperator();
+        calculateResult();
+        System.out.println("result = " + result);
     }
 
     // 양의 정수 2개를 입력받는 메서드
@@ -28,5 +31,24 @@ public class CaculatorLv1 {
         System.out.print("사칙연산 기호를 입력하세요 (+, -, *, / 가능): ");
         operator = sc.nextLine().charAt(0);
         System.out.println("입력한 기호: " + operator);
+    }
+
+    // 입력된 정수와 연산 기호를 사용해 계산하는 메서드
+    private int calculateResult() {
+        switch(operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num1 / num2;
+                break;
+        }
+        return result;
     }
 }

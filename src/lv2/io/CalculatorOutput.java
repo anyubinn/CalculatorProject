529107d0c24b2ad9498214cd411c9a39efeb1f6b
anyubinn <anyubin0724@naver.com>
2025-02-27 17:04:03 +0900

    feat: Lv2. 누적 결과 출력 기능 추가

--- src/lv2/io/CalculatorOutput.java
+++ src/lv2/io/CalculatorOutput.java
@@ -1,5 +1,7 @@
 package lv2.io;
 
+import java.util.List;
+
 public class CalculatorOutput {
 
     public static void printDivider() {
@@ -9,6 +11,13 @@ public class CalculatorOutput {
     public static void printOneResult(int num1, int num2, char operator, Integer result) {
         if (result != null) {
             System.out.println(num1 + " " + operator + " " + num2 + " = " + result);
+        }
+    }
+
+    public static void printAllResults(List<String> results) {
+        System.out.println("누적 결과: ");
+        for (String result : results) {
+            System.out.println(result);
         }
     }
 }

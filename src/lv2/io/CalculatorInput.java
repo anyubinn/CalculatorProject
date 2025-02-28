47a96108035c6ce80ee3ffc4e0f45af7684dc301
anyubinn <anyubin0724@naver.com>
2025-02-28 15:58:10 +0900

    fix: Lv2. 예외처리 수정
    - 숫자 입력 시 공백에 대한 예외처리 추가
    - 연산기호 입력 시 잘못된 사칙연산 기호에 대한 예외처리 추가
    - InputMismatchException -> NumberFormatException으로 변경

--- src/lv2/io/CalculatorInput.java
+++ src/lv2/io/CalculatorInput.java
@@ -6,21 +6,26 @@ import java.util.Set;
 
 public class CalculatorInput {
     static Scanner sc = new Scanner(System.in);
-    static Set<Character> operators = Set.of('+', '-', '*', '/');
+    static Set<String> operators = Set.of("+", "-", "*", "/");
 
     public static int inputNumber() {
         while (true) {
             try {
                 System.out.print("양의 정수를 입력하세요(0부터 가능): ");
-                int num = sc.nextInt();
+                String numStr = sc.nextLine().trim();
+                if (numStr.isEmpty()) {
+                    System.out.println("숫자를 입력해주세요.");
+                    continue;
+                }
+
+                int num = Integer.parseInt(numStr);
                 if (num < 0) {
                     System.out.println("음수는 입력할 수 없습니다. 다시 입력해주세요.");
                     continue;
                 }
                 return num;
-            } catch (InputMismatchException e) {
-                System.out.println("문자는 입력할 수 없습니다. 다시 입력해주세요.");
-                sc.nextLine();
+            } catch (NumberFormatException e) {
+                System.out.println("적절하지 않은 숫자 형식입니다. 다시 입력해주세요.");
             }
         }
     }
@@ -28,18 +33,18 @@ public class CalculatorInput {
     public static char inputOperator() {
         while (true) {
             System.out.print("사칙연산 기호를 입력하세요(+, -, *, / 가능): ");
-            char operator = sc.next().charAt(0);
-            if (!operators.contains(operator)) {
+            String operator = sc.nextLine();
+            if (operator.length() != 1 || !operators.contains(operator)) {
                 System.out.println("잘못된 사칙연산 기호입니다. 다시 입력해주세요.");
                 continue;
             }
-            return operator;
+            return operator.charAt(0);
         }
     }
 
     public static String inputAgain() {
         System.out.print("계속하려면 아무 키나 입력하고 종료하려면 'exit' 키를 입력하세요: ");
-        String input = sc.next();
+        String input = sc.nextLine();
         return input;
     }
 }

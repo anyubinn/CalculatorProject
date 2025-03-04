b02e2ec7ab57de13bbc863f9442b92467f4749e2
anyubinn <anyubin0724@naver.com>
2025-03-04 19:18:34 +0900

    refactor: Lv2. Main 클래스 위치 이동

---
+++ src/lv2/Main.java
@@ -0,0 +1,34 @@
+package lv2;
+
+import java.util.List;
+import lv2.io.CalculatorInput;
+import lv2.io.CalculatorOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        String input = "";
+        Calculator cal = new Calculator();
+        while (!input.equals("exit")){
+            CalculatorOutput.printDivider();
+            CalculatorOutput.printDivider();
+
+            int num1 = CalculatorInput.inputNumber();
+            int num2 = CalculatorInput.inputNumber();
+            CalculatorOutput.printDivider();
+
+            char operator = CalculatorInput.inputOperator();
+            CalculatorOutput.printDivider();
+
+            Integer result = cal.calculate(num1, num2, operator);
+            CalculatorOutput.printOneResult(num1, num2, operator, result);
+            CalculatorOutput.printDivider();
+
+            List<String> results = cal.getResults();
+            CalculatorOutput.printAllResults(results);
+
+            CalculatorOutput.printDivider();
+            CalculatorOutput.printDivider();
+            input = CalculatorInput.inputAgain();
+        }
+    }
+}

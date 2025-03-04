61d3cda002ec4dfbbfa3e9ea5d8586fae3fd8119
anyubinn <anyubin0724@naver.com>
2025-03-04 19:28:15 +0900

    feat: Lv3. Main 클래스 및 Input 클래스 추가

---
+++ src/lv3/Main.java
@@ -0,0 +1,16 @@
+package lv3;
+
+import lv3.io.CalculatorInput;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ArithmeticCalculator arithmeticCalculator = new ArithmeticCalculator();
+        Number num1 = CalculatorInput.inputNumber();
+        Number num2 = CalculatorInput.inputNumber();
+        char operator = CalculatorInput.inputOperator();
+        Number result = arithmeticCalculator.calculate(num1, num2, operator);
+        System.out.println(result);
+    }
+}
--- src/lv3/io/CalculatorInput.java
+++ src/lv3/io/CalculatorInput.java
@@ -1,2 +1,49 @@
-package lv3.io;public class CalculatorInput {
+package lv3.io;
+
+import java.util.Scanner;
+import java.util.Set;
+
+public class CalculatorInput {
+    static Scanner sc = new Scanner(System.in);
+    static Set<String> operators = Set.of("+", "-", "*", "/");
+
+    public static Number inputNumber() {
+        while (true) {
+            try {
+                System.out.print("양의 정수를 입력하세요(0부터 가능): ");
+                String numStr = sc.nextLine().trim();
+                if (numStr.isEmpty()) {
+                    System.out.println("숫자를 입력해주세요.");
+                    continue;
+                }
+
+                Number num = Double.valueOf(numStr);
+                if (num.doubleValue() < 0) {
+                    System.out.println("음수는 입력할 수 없습니다. 다시 입력해주세요.");
+                    continue;
+                }
+                return num;
+            } catch (NumberFormatException e) {
+                System.out.println("적절하지 않은 숫자 형식입니다. 다시 입력해주세요.");
+            }
+        }
+    }
+
+    public static char inputOperator() {
+        while (true) {
+            System.out.print("사칙연산 기호를 입력하세요(+, -, *, / 가능): ");
+            String operator = sc.nextLine();
+            if (operator.length() != 1 || !operators.contains(operator)) {
+                System.out.println("잘못된 사칙연산 기호입니다. 다시 입력해주세요.");
+                continue;
+            }
+            return operator.charAt(0);
+        }
+    }
+
+    public static String inputAgain() {
+        System.out.print("계속하려면 아무 키나 입력하고 종료하려면 'exit' 키를 입력하세요: ");
+        String input = sc.nextLine();
+        return input;
+    }
 }

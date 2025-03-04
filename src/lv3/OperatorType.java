3b2092f77506a03952bca05aaa7b3ba420f6f723
anyubinn <anyubin0724@naver.com>
2025-03-04 15:58:37 +0900

    fix: Lv3. enum 수정

--- src/lv3/OperatorType.java
+++ src/lv3/OperatorType.java
@@ -1,18 +1,18 @@
 package lv3;
 
 public enum OperatorType {
-    PLUS((a, b) -> a + b),
-    MINUS((a, b) -> a - b),
-    MULTIPLY((a, b) -> a * b),
-    DIVIDE((a, b) -> a / b);
+    PLUS('+'),
+    MINUS('-'),
+    MULTIPLY('*'),
+    DIVIDE('/');
 
-    private final Calculator calculator;
+    private final char operator;
 
-    OperatorType(Calculator calculator) {
-        this.calculator = calculator;
+    OperatorType(char operator) {
+        this.operator = operator;
     }
 
-    public int calculate(int num1, int num2) {
-        return calculator.calculate(num1, num2);
+    public char getOperator() {
+        return operator;
     }
 }

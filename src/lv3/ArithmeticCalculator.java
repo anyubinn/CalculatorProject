e35c922492198d48005d74772ea604c31edb5c8f
anyubinn <anyubin0724@naver.com>
2025-03-05 10:00:09 +0900

    feat: Lv3. 연산 결과 저장 로직 추가

--- src/lv3/ArithmeticCalculator.java
+++ src/lv3/ArithmeticCalculator.java
@@ -1,8 +1,30 @@
 package lv3;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ArithmeticCalculator {
 
+    private List<Double> results;
+
+    public ArithmeticCalculator() {
+        this.results = new ArrayList<>();
+    }
+
     public <T extends Number> double calculate(T num1, T num2, OperatorType operator) {
-        return operator.calculate(num1, num2);
+        double result = operator.calculate(num1, num2);
+        results.add(result);
+        removeOldOneResults();
+        return result;
+    }
+
+    public List<Double> getResults() {
+        return results;
+    }
+
+    private void removeOldOneResults() {
+        if (results.size() > 3) {
+            results.remove(0);
+        }
     }
 }

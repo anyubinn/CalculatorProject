48bb12ea17ef73f183695792e7ad05147f0fb190
anyubinn <anyubin0724@naver.com>
2025-03-05 10:37:11 +0900

    feat: Lv3. 계산기 반복 실행 및 종료 조건 추가

--- src/lv3/Main.java
+++ src/lv3/Main.java
@@ -6,16 +6,21 @@ public class Main {
 
     public static void main(String[] args) {
 
+        String input = "";
         ArithmeticCalculator arithmeticCalculator = new ArithmeticCalculator();
-        Number num1 = CalculatorInput.inputNumber();
-        Number num2 = CalculatorInput.inputNumber();
-        OperatorType operator = CalculatorInput.inputOperator();
-        Number result = null;
-        try {
-            result = arithmeticCalculator.calculate(num1, num2, operator);
-        } catch (ArithmeticException e) {
-            System.out.println(e.getMessage());
+        while (!input.equals("exit")) {
+            Number num1 = CalculatorInput.inputNumber();
+            Number num2 = CalculatorInput.inputNumber();
+            OperatorType operator = CalculatorInput.inputOperator();
+            Number result = null;
+            try {
+                result = arithmeticCalculator.calculate(num1, num2, operator);
+            } catch (ArithmeticException e) {
+                System.out.println(e.getMessage());
+            }
+            System.out.println(result);
+
+            input = CalculatorInput.inputAgain();
         }
-        System.out.println(result);
     }
 }
